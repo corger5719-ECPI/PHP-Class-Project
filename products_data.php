095daeb8ce4786971eafee13ea5f2e53b575abe4
corger5719-ECPI <cora.germany@gmail.com>
2026-02-08 13:11:48 -0500

    Add files via upload

---
+++ products_data.php
@@ -0,0 +1,31 @@
+<?php
+// includes/products_data.php
+function getProducts(): array {
+    return [
+        'NH001' => [
+            'name' => 'Moisturizing Shampoo',
+            'description' => 'Gentle, sulfate-free shampoo for natural hair',
+            'price' => 11.99
+        ],
+        'NH002' => [
+            'name' => 'Hydrating Conditioner',
+            'description' => 'Deep moisture conditioner for curls and coils',
+            'price' => 12.49
+        ],
+        'NH003' => [
+            'name' => 'Leave-In Conditioner',
+            'description' => 'Lightweight leave-in to soften and detangle hair',
+            'price' => 10.99
+        ],
+        'NH004' => [
+            'name' => 'Curl Defining Cream',
+            'description' => 'Enhances curl definition and reduces frizz',
+            'price' => 13.99
+        ],
+        'NH005' => [
+            'name' => 'Natural Hair Growth Oil',
+            'description' => 'Nourishes scalp and supports healthy hair growth',
+            'price' => 9.99
+        ],
+    ];
+}
