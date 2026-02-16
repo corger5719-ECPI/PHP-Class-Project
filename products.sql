8a1b82fb78eb7de55798967c4a4054a3584aef56
corger5719-ECPI <cora.germany@gmail.com>
2026-02-15 21:49:53 -0500

    Clean up products.sql by removing metadata
    Removed unnecessary SQL dump metadata and transaction commands.

--- products.sql
+++ products.sql
@@ -1,28 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 08, 2026 at 12:37 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `sdc310_germany`
---
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `products`
 --
@@ -63,8 +38,4 @@ ALTER TABLE `products`
 --
 ALTER TABLE `products`
   MODIFY `ProductNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
