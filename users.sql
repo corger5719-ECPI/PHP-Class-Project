40f3fba52147f6cbce3a233bde7389ee34b448a6
CG <cora.germany@gmail.com>
2026-02-22 08:59:17 -0500

    Refactor users.sql to remove backticks and comments

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE users (
   `UserNo` int(11) NOT NULL,
   `UserId` varchar(15) NOT NULL,
   `LName` varchar(25) NOT NULL,
@@ -14,29 +14,10 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`UserNo`, `UserId`, `LName`, `FName`) VALUES
+INSERT INTO users (UserNo, UserId, LName, FName) VALUES
 (0, 'CLG', 'Germany', 'Cora'),
 (2, 'UB', 'Braun', 'Un'),
 (4, 'BW', 'Wojok', 'Bob'),
 (5, 'CC', 'Conway', 'Cara');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`UserNo`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `UserNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
