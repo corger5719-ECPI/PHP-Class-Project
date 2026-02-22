1756a40f9a788cf208ac2d4b5fbec8dfb12d4596
CG <cora.germany@gmail.com>
2026-02-22 08:56:30 -0500

    Fix SQL syntax and update product entries

--- products.sql
+++ products.sql
@@ -1,41 +1,14 @@
---
--- Table structure for table `products`
---
-
 CREATE TABLE `products` (
-  `ProductNo` int(11) NOT NULL, Primary, Auto Increment,
+  `ProductNo` int(11) NOT NULL, PRIMARY, AUT0_INCREMENT,
   `ProductId` varchar(10) NOT NULL,
   `ProductName` varchar(50) NOT NULL,
   `ProductDescription` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=geostd8 COLLATE=geostd8_bin;
+) 
 
---
--- Dumping data for table `products`
---
-
-INSERT INTO `products` (`ProductNo`, `ProductId`, `ProductName`, `ProductDescription`) VALUES
+INSERT INTO products (ProductNo, ProductId, ProductName, ProductDescription) VALUES
 (1, 'hcomb', 'Hot Comb', 'hotcomb that straightens hair'),
+(2, 'hgrease', 'Hair Grease', 'locks in moisture & creates a protective barrier & adds sheen'), 
 (3, 'mspray', 'Moisturizing Spray', 'hair moisturizer in spray bottle'),
-(4, 'blkextensi', 'Braiding Hair', '14-inch human hair extension in color black'),
+(4, 'blkextn', 'Braiding Hair', '14-inch human hair extension in color black'),
 (5, 'combbrush', 'Comb & Brush Set', 'hair comb & brush set');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`ProductNo`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `ProductNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
